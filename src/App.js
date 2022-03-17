2373679a8a15195f9ffd5b20130259e13d6bb1d2
Manish bharti <parasparas0001@gmail.com>
2022-03-17 09:55:54 +0530

    added html-react parser npm package and required experiment code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Parse, { domToReact } from "html-react-parser";
+import React from "react";
 
 function App() {
+  const options = {
+    replace: ({ attribs, children }) => {
+      if (attribs && attribs.class === "test") {
+        return <Check>{domToReact(children, options)}</Check>;
+      }
+    },
+  };
+  const htmlString =
+    "<table class='test'><tr><td>hello</td></tr><tr><td>hi</td></tr><tr><td>hello</td></tr><tr><td>hi</td></tr><tr><td>hello</td></tr><tr><td>hi</td></tr><tr><td>hello</td></tr><tr><td>hi</td></tr><tr><td>hello</td></tr><tr><td>hi</td></tr></table>";
+
+  const parsedString = Parse(htmlString, options);
+
+  console.log(parsedString);
+
   return (
     <div className="App">
+      {parsedString}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -20,6 +37,36 @@ function App() {
       </header>
     </div>
   );
+}
+
+class Check extends React.PureComponent {
+  state = {
+    show: 0,
+  };
+  render() {
+    let child = { ...this.props.children };
+    child = { ...child.props.children };
+    let only = [];
+    let i = 0;
+    for (let el in child) {
+    
+      if (i < this.state.show) only.push(child[el]);
+      i++;
+    }
+
+    console.log(only);
+
+    return (
+      <div className="container">
+      
+
+        <table>
+          <tbody>{only}</tbody>
+        </table>
+        <button onClick={()=>{this.setState({show:this.state.show+2})}}className="ok">on Click</button>
+      </div>
+    );
+  }
 }
 
 export default App;
